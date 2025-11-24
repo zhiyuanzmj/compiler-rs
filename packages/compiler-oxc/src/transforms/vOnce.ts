f92a1ecd9eb3789bbd9c7bb7bf2cebfbe2b650f9
zhiyuanzmj <260480378@qq.com>
2025-11-25 00:01:19 +0800

    chore: remove compiler-oxc

--- packages/compiler-oxc/src/transforms/vOnce.ts
+++
@@ -1,8 +0,0 @@
-import { findProp } from '../utils'
-import type { NodeTransform } from '../transform'
-
-export const transformVOnce: NodeTransform = (node, context) => {
-  if (node.type === 'JSXElement' && findProp(node, 'v-once')) {
-    context.inVOnce = true
-  }
-}
--- packages/compiler-oxc/tsdown.config.ts
+++
@@ -1,5 +0,0 @@
-import { config } from '../../tsdown.config'
-
-export default config({
-  entry: ['src/index.ts'],
-})
